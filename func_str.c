e974543c6ab06bf5b3cf2ea4494a15cbee95c17b
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-12 09:56:20 +0800

    move files

--- func_str.c
+++ func_str.c
@@ -1,85 +1,102 @@
 #include "monty.h"
-/**
- * _strcmp - Function that compares two strings.
- * @s1: type str compared
- * @s2: type str compared
- * Return: 0 if s1 and s2 are equals.
- *         another value if they are different
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
 
-	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
-		;
-	if (s1[i] > s2[i])
-		return (1);
-	if (s1[i] < s2[i])
-		return (-1);
-	return (0);
+/**
+ * get_int - get char pointer to new str with int
+ * @num: number to convert to string
+ * Return: char pointer converted as null if fails and malloc.
+ */
+char *get_int(int num)
+{
+	long num_v = 0;
+	unsigned int tmp;
+	int len = 0;
+	char *ret;
+
+	tmp = abs_m(num);
+	len = len_buff_uint(tmp, 10);
+
+	if (num < 0 || num_v < 0)
+		len++;
+	ret = malloc(len + 1);
+	if (!ret)
+		return (NULL);
+
+	full_buff(tmp, 10, ret, len);
+	if (num < 0 || num_v < 0)
+		ret[0] = '-';
+
+	return (ret);
+}
+/**
+ * abs_m - abs value of int
+ * @i: integer to get absolute value of
+ * Return: unsigned int of i
+ */
+unsigned int abs_m(int i)
+{
+	if (i < 0)
+		return (-(unsigned int)i);
+	return ((unsigned int)i);
+}
+/**
+ * len_buff_uint - length of buff for an unsigned int
+ * @num: number to get length needed for
+ * @base: base of number representation used by buffer
+ * Return: int of len of buff
+ */
+int len_buff_uint(unsigned int num, unsigned int base)
+{
+	int len = 1;
+
+	while (num > base - 1)
+	{
+		len++;
+		num /= base;
+	}
+	return (len);
+}
+/**
+ * full_buff - complete buff with numbers
+ * @num: number to convert to str
+ * @base: b number
+ * @buff: buffer to be completed
+ * @buff_size: size of buffer
+ * Return: void.
+ */
+void full_buff(unsigned int num, unsigned int base, char *buff, int buff_size)
+{
+	int rem, i = buff_size - 1;
+
+	buff[buff_size] = '\0';
+	while (i >= 0)
+	{
+		rem = num % base;
+		if (rem > 9)
+			buff[i] = rem + 87;
+		else
+			buff[i] = rem + '0';
+		num /= base;
+		i--;
+	}
 }
 
 /**
- * _sch - search if a char is inside a string
- * @s: string to review
- * @c: char to find
- * Return: 1 if success 0 if not
+ * _isdigit - Entry point
+ *
+ * Return: if is digit  or not, return 1 else 0
  */
-int _sch(char *s, char c)
+int _isdigit(void)
 {
-	int cont = 0;
+	int i = 0;
 
-	while (s[cont] != '\0')
+	if (globalvar.token2[0] == '-' || globalvar.token2[0] == '+')
+		i++;
+	while (globalvar.token2[i])
 	{
-		if (s[cont] == c)
-		{
-			break;
-		}
-		cont++;
-	}
-	if (s[cont] == c)
-		return (1);
-	else
-		return (0);
-}
 
-/**
- * _strtoky - function that cut a string into tokens depending of the delimit
- * @s: string to cut in parts
- * @d: delimiters
- * Return: first partition
- */
-char *_strtoky(char *s, char *d)
-{
-	static char *ultimo;
-	int i = 0, j = 0;
-
-	if (!s)
-		s = ultimo;
-	while (s[i] != '\0')
-	{
-		if (_sch(d, s[i]) == 0 && s[i + 1] == '\0')
-		{
-			ultimo = s + i + 1;
-			*ultimo = '\0';
-			s = s + j;
-			return (s);
-		}
-		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 0)
-			i++;
-		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 1)
-		{
-			ultimo = s + i + 1;
-			*ultimo = '\0';
-			ultimo++;
-			s = s + j;
-			return (s);
-		}
-		else if (_sch(d, s[i]) == 1)
-		{
-			j++;
-			i++;
-		}
+		if (isdigit(globalvar.token2[i]) == 0)
+			return (0);
+		i++;
 	}
-	return (NULL);
+	return (1);
 }
