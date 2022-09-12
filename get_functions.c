2be9ed0cef3e00d856c6478933dfb87b9ff4be94
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-12 09:52:18 +0800

    move files

--- get_functions.c
+++ get_functions.c
@@ -1,135 +1,52 @@
 #include "monty.h"
 
 /**
- * push - Add node to the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
+ * get_builtin - Parses the instruction with the function.
+ * @token: type pointer extern var char
+ * @stack: type pointer node of data struct
+ * @line_number: line of instructions
+ * Return: Retunr the funtion
  */
-void push(stack_t **stack, unsigned int line_number)
+int get_builtin(char *token, stack_t **stack, unsigned int line_number)
 {
+	instruction_t op_built[] = {
+		{ "push", push },
+		{ "pall", pall},
+		{ "pop", pop },
+		{ "add", add },
+		{ "nop", nop },
+		{ "sub", sub },
+		{ "pall", pall },
+		{ "pint", pint },
+		{ "swap", swap },
+		{ "mul", mul },
+		{ "div", div_m },
+		{ "pchar", pchar },
+		{ "mod", mod_m },
+		{ "\n", nop },
+		{ " ", nop },
+		{ "\t", nop },
+		{ "pstr", pstr_t },
+		{ "rotl", rotrl },
+		{ "rotr", rotr },
+		{ NULL, NULL }
+	};
+	int i, flag = 0;
+	char *argumentos = NULL;
 
-	int n = 0;
-
-	if (globalvar.token2 == NULL)
+	if (stack == NULL || token == NULL)
+		return (0);
+	argumentos = token;
+	for (i = 0; op_built[i].opcode; i++)
 	{
-		free_dlistint(*stack);
-		stderr_int(line_number);
+		if (strcmp(argumentos, op_built[i].opcode) == 0)
+		{
+			flag = 1;
+			op_built[i].f(stack, line_number);
+			break;
+		}
 	}
-	if (!_isdigit() || stack == NULL)
-	{
-		free_dlistint(*stack);
-		stderr_int(line_number);
-	}
-	n = atoi(globalvar.token2);
-	if (*stack  == NULL)
-	{
-		create_node_stackfirst(stack, n);
-	}
-	else
-	{
-		create_node_stackend(stack, n);
-	}
-}
-
-/**
- * pall - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-
-	stack_t *temp = NULL;
-
-
-	if (*stack == NULL)
-	{
-		return;
-	}
-	if (*stack == NULL && line_number != 1)
-	{
-		free_dlistint(*stack);
-		free_globalvars();
-		exit(EXIT_SUCCESS);
-	}
-	temp = *stack;
-	while (temp->next != NULL)
-		temp = temp->next;
-	while (temp->prev != NULL)
-	{
-		printf("%d", temp->n);
-		temp = temp->prev;
-		printf("\n");
-	}
-	printf("%d\n", temp->n);
-}
-
-
-/**
- * pint - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void pint(stack_t **stack, unsigned int line_number)
-{
-
-	stack_t *temp = NULL;
-
-	if (stack == NULL || *stack == NULL)
-	{
-		pint_e(line_number);
-		return;
-	}
-	temp = *stack;
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	printf("%d", temp->n);
-	printf("\n");
-}
-
-/**
- * swap - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void swap(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
-	int i, j;
-
-	if (*stack == NULL || stack == NULL)
-		op_e(line_number, "swap");
-
-	temp = (*stack)->next;
-	if ((*stack)->next == NULL)
-		op_e(line_number, "swap");
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	i = temp->n;
-	j = temp->prev->n;
-	temp->n = j;
-	temp->prev->n = i;
-}
-/**
- * nop - does not do anything
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void nop(stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
+	if (flag == 0)
+		stderr_unknown(token, line_number);
+	return (0);
 }
